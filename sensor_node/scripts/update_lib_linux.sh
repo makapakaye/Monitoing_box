85bca275488444847517dd3c5ce0f1cea10d7650
hmes98318 <52697423+hmes98318@users.noreply.github.com>
2024-05-14 22:21:17 +0800

    chore: Add update library script for sensor_node

---
+++ sensor_node/scripts/update_lib_linux.sh
@@ -0,0 +1,4 @@
+rm -rf ~/Documents/Arduino/libraries/BME68x_Sensor_library && \
+rm -rf ~/Documents/Arduino/libraries/bsec2
+
+cp -r ~/Documents/GitHub/Monitoing_box/sensor_node/lib/* ~/Documents/Arduino/libraries/
