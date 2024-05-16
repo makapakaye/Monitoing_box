36742557fc924ece95d95217fd86044528d2cf84
hmes98318 <52697423+hmes98318@users.noreply.github.com>
2024-05-17 03:29:09 +0800

    chore: Add test file for sensor_node

--- sensor_node/test/sensor_test/sensor_test.ino
+++ sensor_node/test/sensor_test/sensor_test.ino
@@ -1,164 +1,67 @@
 /**
- * Sensor test
+ * 單顆感測器用 BME68X
  */
 
 #include <heltec_unofficial.h>
-#include <bsec2.h>
-#include <commMux/commMux.h>
+#include <Wire.h>
+
+#include "src/lib/BME680.h"
 
 
-/* Macros used */
-#define STATE_SAVE_PERIOD UINT32_C(360 * 60 * 1000) /* 360 minutes - 4 times a day */
+// SPI
+/*
+#define BME_SCK 18
+#define BME_MISO 19
+#define BME_MOSI 23
+#define BME_CS 5
+*/
 
-/* Number of sensors to operate*/
-#define NUM_OF_SENS 1           // 8
-#define SENSOR_OPMODE BME68X_SEQUENTIAL_MODE
+// I2C
+#define BME_SDA_PIN 16 // 23 藍
+#define BME_SCL_PIN 17 // 22 紫
 
 /* Error LED setup */
-#define PANIC_LED LED_PIN       // LED_PIN from <heltec_unofficial.h>
+#define PANIC_LED LED_PIN // LED_PIN from <heltec_unofficial.h>
 #define ERROR_DURATION 1000
 
-
-/* Helper functions declarations */
-/**
- * @brief : This function toggles the led continuously with one second delay
- */
-void errLeds(void);
-
-/**
- * @brief : This function updates/saves BSEC state
- * @param[in] bsec  : Bsec2 class object
- */
-void updateBsecState(Bsec2 bsec);
-
-/**
- * @brief : This function checks the BSEC status, prints the respective error code. Halts in case of error
- * @param[in] bsec  : Bsec2 class object
- */
-void checkBsecStatus(Bsec2 bsec);
+BME680 bme(Wire); // I2C
+// BME680 bme(BME_CS); // SPI
+// BME680 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK);
 
 
-/********************************************************* */
-/*                  Test function                          */
-/********************************************************* */
-
-/**
- * 獲取感測器資料
- * @param[in] bsec      : Instance of sensor
- */
-bsecOutputs getSensorData(Bsec2 &envSensor);
-
-/**
- * Print 獲取的感測器資料
- * @param[in] bsec      : Instance of sensor
-*/
-void printSensorData(Bsec2 bsec);
-
-/********************************************************* */
-/*                END Test function                        */
-/********************************************************* */
-
-
-/* Create an object of the class Bsec2 */
-Bsec2 envSensor[NUM_OF_SENS];
-comm_mux commConfig[NUM_OF_SENS];
-uint8_t bsecMemBlock[NUM_OF_SENS][BSEC_INSTANCE_SIZE];
-
-uint8_t sensor = 0;
-unsigned long current_time = 0;
-
-
-/* Entry point for the example */
-void setup(void)
+typedef struct
 {
-    /* Desired subscription list of BSEC2 outputs */
-    bsecSensor sensorList[] = {
-        BSEC_OUTPUT_RAW_TEMPERATURE,
-        BSEC_OUTPUT_RAW_PRESSURE,
-        BSEC_OUTPUT_RAW_HUMIDITY,
-        BSEC_OUTPUT_RAW_GAS,
-        BSEC_OUTPUT_RAW_GAS_INDEX};
+    float temperature;
+    float humidity;
+    float pressure;
+    float gasResistance;
+} BME680Data;
 
-    Serial.begin(115200);
-    // EEPROM.begin(BSEC_MAX_STATE_BLOB_SIZE + 1);
+BME680Data sensorData;
 
-    /* Initiate SPI communication */
-    comm_mux_begin(Wire, SPI);
 
-    pinMode(PANIC_LED, OUTPUT);
-    delay(100);
+unsigned long lastTime = 0;
+unsigned long timerDelay = 1000; // send readings timer
 
-    /* Valid for boards with USB-COM. Wait until the port is open */
-    while (!Serial) delay(10);
-
-    for (uint8_t i = 0; i < NUM_OF_SENS; i++)
+void getBME680Readings()
+{
+    // Tell BME680 to begin measurement.
+    unsigned long endTime = bme.beginReading();
+    if (endTime == 0)
     {
-        /* Sets the Communication interface for the given sensor */
-        commConfig[i] = comm_mux_set_config(Wire, SPI, i, commConfig[i]);
-
-        /* Assigning a chunk of memory block to the bsecInstance */
-        envSensor[i].allocateMemory(bsecMemBlock[i]);
-
-        /* Initialize the library and interfaces */
-        if (!envSensor[i].begin(BME68X_SPI_INTF, comm_mux_read, comm_mux_write, comm_mux_delay, &commConfig[i]))
-        {
-            checkBsecStatus(envSensor[i]);
-        }
-
-        /* Subscribe for the desired BSEC2 outputs */
-        if (!envSensor[i].updateSubscription(sensorList, ARRAY_LEN(sensorList), BSEC_SAMPLE_RATE_SCAN))
-        {
-            checkBsecStatus(envSensor[i]);
-        }
-
-
-        updateBsecState(envSensor[i]);
-
-        while (!envSensor[i].run())
-        {
-            Serial.println("+++ Not run +++");
-            // checkBsecStatus(envSensor[sensor]);
-            delay(50);
-        }
-
-        envSensor[i].setSensorOpMode(SENSOR_OPMODE);
-        Serial.println("envSensor opmode " + String(envSensor[i].getSensorOpMode()));
+        Serial.println(F("Failed to begin reading :("));
+        errLeds();
+    }
+    if (!bme.endReading())
+    {
+        Serial.println(F("Failed to complete reading :("));
+        errLeds();
     }
 
-
-    Serial.println("\nBSEC library version " +
-                   String(envSensor[0].version.major) + "." +
-                   String(envSensor[0].version.minor) + "." +
-                   String(envSensor[0].version.major_bugfix) + "." +
-                   String(envSensor[0].version.minor_bugfix));
-
-    Serial.println("----------");
-    delay(1000);
-}
-
-void loop(void)
-{
-    for (sensor = 0; sensor < NUM_OF_SENS; sensor++)
-    {
-        while (!envSensor[sensor].run())
-        {
-            // checkBsecStatus(envSensor[sensor]);  // 不檢查錯誤 循環跑到有東西
-            delay(50);
-        }
-
-        if (envSensor[sensor].getSensorStatus() != BME68X_OK && envSensor[sensor].getSensorStatus() != BME68X_W_NO_NEW_DATA)
-        {
-            // 感測器設置有問題 無法獲取資訊
-            errLeds();
-        }
-
-        current_time = millis();
-        Serial.println("current_time: " + String(current_time) + " ms");
-        printSensorData(envSensor[sensor]);
-    }
-
-    Serial.println("--- sleep ---");
-    delay(3000);
+    sensorData.temperature = bme.temperature;
+    sensorData.pressure = bme.pressure / 100.0;
+    sensorData.humidity = bme.humidity;
+    sensorData.gasResistance = bme.gas_resistance / 1000.0;
 }
 
 void errLeds(void)
@@ -172,106 +75,119 @@ void errLeds(void)
     }
 }
 
-bsecOutputs getSensorData(Bsec2 &envSensor)
+void setup()
 {
-    const bsecOutputs *ptrOutputs = envSensor.getOutputs();
+    Serial.begin(115200);
 
-    bsecOutputs outputs;
+    bme.setSPIPins(BME_SDA_PIN, BME_SCL_PIN);
 
-    if (!ptrOutputs)
+    // Init BME680 sensor
+    if (!bme.begin())
     {
-        // 如果指標為空，則傳回空的 outputs
-        memset(&outputs, 0, sizeof(outputs));
-        return outputs;
+        Serial.println(F("Could not find a valid BME680 sensor, check wiring!"));
+        errLeds();
     }
 
-    // 複製指標所指向的內容到 outputs 進行回傳
-    memcpy(&outputs, ptrOutputs, sizeof(bsecOutputs));
+    // Set up oversampling and filter initialization
+    bme.setTemperatureOversampling(BME680_OS_8X);
+    bme.setHumidityOversampling(BME680_OS_2X);
+    bme.setPressureOversampling(BME680_OS_4X);
+    bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+    bme.setGasHeater(320, 150); // 320*C for 150 ms
 
-    // Serial.println("getSensorData return outputs");
-    return outputs;
+    Serial.println("----- init ok -----");
 }
 
-void printSensorData(Bsec2 bsec)
+void loop()
 {
-    const bsecOutputs outputs = getSensorData(bsec);
+    heltec_loop();
 
-    if (!outputs.nOutputs)
+    if (button.isSingleClick())
     {
-        // Serial.println("printSensorData No data");
-        return;
-    }
-
-    Serial.println("BSEC outputs:\n\tsensor num = " + String(sensor));
-    Serial.println("\ttimestamp = " + String((int)(outputs.output[0].time_stamp / INT64_C(1000000))));
-
-    int index = 0;
-
-    for (uint8_t i = 0; i < outputs.nOutputs; i++)
-    {
-        const bsecData output = outputs.output[i];
-        switch (output.sensor_id)
+        for (int n = 0; n <= 100; n++)
         {
-        case BSEC_OUTPUT_RAW_TEMPERATURE:
-            Serial.println("\ttemperature = " + String(output.signal));
-            break;
-        case BSEC_OUTPUT_RAW_PRESSURE:
-            Serial.println("\tpressure = " + String(output.signal));
-            break;
-        case BSEC_OUTPUT_RAW_HUMIDITY:
-            Serial.println("\thumidity = " + String(output.signal));
-            break;
-        case BSEC_OUTPUT_RAW_GAS:
-            Serial.println("\tgas resistance = " + String(output.signal));
-            break;
-        case BSEC_OUTPUT_RAW_GAS_INDEX:
-            Serial.println("\tgas index = " + String(output.signal));
-            break;
-        default:
-            break;
+            heltec_led(n);
+            delay(5);
+        }
+
+        getBME680Readings();
+        Serial.printf("Temperature = %.2f ºC \n", sensorData.temperature);
+        Serial.printf("Humidity = %.2f % \n", sensorData.humidity);
+        Serial.printf("Pressure = %.2f hPa \n", sensorData.pressure);
+        Serial.printf("Gas Resistance = %.2f KOhm \n", sensorData.gasResistance);
+        Serial.println("--------------");
+
+        for (int n = 100; n >= 0; n--)
+        {
+            heltec_led(n);
+            delay(5);
         }
     }
 }
 
-void updateBsecState(Bsec2 bsec)
+
+/*
+int val = 0;
+void loop()
 {
-    static uint16_t stateUpdateCounter = 0;
-    bool update = false;
+    heltec_loop();
 
-    if (!stateUpdateCounter || (stateUpdateCounter * STATE_SAVE_PERIOD) < millis())
+    if (button.isSingleClick())
     {
-        /* Update every STATE_SAVE_PERIOD minutes */
-        update = true;
-        stateUpdateCounter++;
+        val = 1;
+    }
+    if (button.isDoubleClick())
+    {
+        val = 3;
     }
 
-    if (update)
+    switch (val)
     {
-        checkBsecStatus(bsec);
+
+    case 1: // scan data
+    {
+        for (int n = 0; n <= 100; n++)
+        {
+            heltec_led(n);
+            delay(5);
+        }
+
+        getBME680Readings();
+
+        for (int n = 100; n >= 0; n--)
+        {
+            heltec_led(n);
+            delay(5);
+        }
+
+        val = 2;
+        Serial.println("getBME680Readings OK");
+        delay(2000);
+        break;
     }
+    
+    case 2: // print data
+    {
+        Serial.printf("Temperature = %.2f ºC \n", sensorData.temperature);
+        Serial.printf("Humidity = %.2f % \n", sensorData.humidity);
+        Serial.printf("Pressure = %.2f hPa \n", sensorData.pressure);
+        Serial.printf("Gas Resistance = %.2f KOhm \n", sensorData.gasResistance);
+        delay(5000);
+        val = 0;
+        break;
+    }
+
+    case 3: // deep sleep
+    {
+        Serial.printf("deep sleep");
+        heltec_deep_sleep(5);
+        break;
+    }
+
+    default:
+        break;
+    }
+
+    Serial.println("--- loop ---");
 }
-
-void checkBsecStatus(Bsec2 bsec)
-{
-    // BSEC
-    if (bsec.status < BSEC_OK)
-    {
-        Serial.println("BSEC error code : " + String(bsec.status));
-        errLeds(); /* Halt in case of failure */
-    }
-    else if (bsec.status > BSEC_OK)
-    {
-        Serial.println("BSEC warning code : " + String(bsec.status));
-    }
-
-    // Sensor
-    if (bsec.sensor.status < BME68X_OK)
-    {
-        Serial.println("BME68X error code : " + String(bsec.sensor.status));
-        errLeds(); /* Halt in case of failure */
-    }
-    else if (bsec.sensor.status > BME68X_OK)
-    {
-        Serial.println("BME68X warning code : " + String(bsec.sensor.status));
-    }
-}
+*/
---
+++ sensor_node/test/x8_sensor_test/x8_sensor_test.ino
@@ -0,0 +1,281 @@
+/**
+ * x8 Sensor test
+ */
+
+#include <heltec_unofficial.h>
+#include <bsec2.h>
+#include <commMux/commMux.h>
+
+
+/* Macros used */
+#define STATE_SAVE_PERIOD UINT32_C(360 * 60 * 1000) /* 360 minutes - 4 times a day */
+
+/* Number of sensors to operate*/
+#define NUM_OF_SENS 1           // 8
+#define SENSOR_OPMODE BME68X_SEQUENTIAL_MODE
+
+/* Error LED setup */
+#define PANIC_LED LED_PIN       // LED_PIN from <heltec_unofficial.h>
+#define ERROR_DURATION 1000
+
+
+/* Helper functions declarations */
+/**
+ * @brief : This function toggles the led continuously with one second delay
+ */
+void errLeds(void);
+
+/**
+ * @brief : This function updates/saves BSEC state
+ * @param[in] bsec  : Bsec2 class object
+ */
+void updateBsecState(Bsec2 bsec);
+
+/**
+ * @brief : This function checks the BSEC status, prints the respective error code. Halts in case of error
+ * @param[in] bsec  : Bsec2 class object
+ */
+void checkBsecStatus(Bsec2 bsec);
+
+
+/********************************************************* */
+/*                  Test function                          */
+/********************************************************* */
+
+/**
+ * 獲取感測器資料
+ * @param[in] bsec      : Instance of sensor
+ */
+bsecOutputs getSensorData(Bsec2 &envSensor);
+
+/**
+ * Print 獲取的感測器資料
+ * @param[in] bsec      : Instance of sensor
+*/
+void printSensorData(Bsec2 bsec);
+
+/********************************************************* */
+/*                END Test function                        */
+/********************************************************* */
+
+
+/* Create an object of the class Bsec2 */
+Bsec2 envSensor[NUM_OF_SENS];
+comm_mux commConfig[NUM_OF_SENS];
+uint8_t bsecMemBlock[NUM_OF_SENS][BSEC_INSTANCE_SIZE];
+
+uint8_t sensor = 0;
+unsigned long current_time = 0;
+
+
+/* Entry point for the example */
+void setup(void)
+{
+    /* Desired subscription list of BSEC2 outputs */
+    bsecSensor sensorList[] = {
+        BSEC_OUTPUT_RAW_TEMPERATURE,
+        BSEC_OUTPUT_RAW_PRESSURE,
+        BSEC_OUTPUT_RAW_HUMIDITY,
+        BSEC_OUTPUT_RAW_GAS,
+        BSEC_OUTPUT_RAW_GAS_INDEX};
+
+    Serial.begin(115200);
+    // EEPROM.begin(BSEC_MAX_STATE_BLOB_SIZE + 1);
+
+    /* Initiate SPI communication */
+    comm_mux_begin(Wire, SPI);
+
+    pinMode(PANIC_LED, OUTPUT);
+    delay(100);
+
+    /* Valid for boards with USB-COM. Wait until the port is open */
+    while (!Serial) delay(10);
+
+    for (uint8_t i = 0; i < NUM_OF_SENS; i++)
+    {
+        /* Sets the Communication interface for the given sensor */
+        commConfig[i] = comm_mux_set_config(Wire, SPI, i, commConfig[i]);
+
+        /* Assigning a chunk of memory block to the bsecInstance */
+        envSensor[i].allocateMemory(bsecMemBlock[i]);
+
+        /* Initialize the library and interfaces */
+        if (!envSensor[i].begin(BME68X_SPI_INTF, comm_mux_read, comm_mux_write, comm_mux_delay, &commConfig[i]))
+        {
+            checkBsecStatus(envSensor[i]);
+        }
+
+        /* Subscribe for the desired BSEC2 outputs */
+        if (!envSensor[i].updateSubscription(sensorList, ARRAY_LEN(sensorList), BSEC_SAMPLE_RATE_SCAN))
+        {
+            checkBsecStatus(envSensor[i]);
+        }
+
+
+        updateBsecState(envSensor[i]);
+
+        while (!envSensor[i].run())
+        {
+            Serial.println("+++ Not run +++");
+            // checkBsecStatus(envSensor[sensor]);
+            delay(50);
+        }
+
+        envSensor[i].setSensorOpMode(SENSOR_OPMODE);
+        Serial.println("envSensor opmode " + String(envSensor[i].getSensorOpMode()));
+    }
+
+
+    Serial.println("\nBSEC library version " +
+                   String(envSensor[0].version.major) + "." +
+                   String(envSensor[0].version.minor) + "." +
+                   String(envSensor[0].version.major_bugfix) + "." +
+                   String(envSensor[0].version.minor_bugfix));
+
+    Serial.println("----------");
+    delay(1000);
+}
+
+void loop(void)
+{
+    for (sensor = 0; sensor < NUM_OF_SENS; sensor++)
+    {
+        while (!envSensor[sensor].run())
+        {
+            delay(50);
+        }
+
+        if (envSensor[sensor].getSensorStatus() != BME68X_OK && envSensor[sensor].getSensorStatus() != BME68X_W_NO_NEW_DATA)
+        {
+            // 感測器設置有問題 無法獲取資訊
+            errLeds();
+        }
+
+        current_time = millis();
+        Serial.println("current_time: " + String(current_time) + " ms");
+        printSensorData(envSensor[sensor]);
+    }
+
+    Serial.println("--- sleep ---");
+    delay(3000);
+}
+
+void errLeds(void)
+{
+    while (1)
+    {
+        digitalWrite(PANIC_LED, HIGH);
+        delay(ERROR_DURATION);
+        digitalWrite(PANIC_LED, LOW);
+        delay(ERROR_DURATION);
+    }
+}
+
+bsecOutputs getSensorData(Bsec2 &envSensor)
+{
+    const bsecOutputs *ptrOutputs = envSensor.getOutputs();
+
+    bsecOutputs outputs;
+
+    if (!ptrOutputs)
+    {
+        // 如果指標為空，則傳回空的 outputs
+        memset(&outputs, 0, sizeof(outputs));
+        return outputs;
+    }
+
+    // 複製指標所指向的內容到 outputs 進行回傳
+    memcpy(&outputs, ptrOutputs, sizeof(bsecOutputs));
+
+    // Serial.println("getSensorData return outputs");
+    return outputs;
+}
+
+void printSensorData(Bsec2 bsec)
+{
+    bsecOutputs outputs = getSensorData(bsec);
+
+    while (!outputs.nOutputs)
+    {
+        // Serial.println("printSensorData No data");
+        return;
+        /*
+        Serial.println("----- printSensorData No data -----");
+        sleep(100);
+        outputs = getSensorData(bsec);
+        */
+    }
+
+    Serial.println("BSEC outputs:\n\tsensor num = " + String(sensor));
+    Serial.println("\ttimestamp = " + String((int)(outputs.output[0].time_stamp / INT64_C(1000000))));
+
+    int index = 0;
+
+    for (uint8_t i = 0; i < outputs.nOutputs; i++)
+    {
+        const bsecData output = outputs.output[i];
+        switch (output.sensor_id)
+        {
+        case BSEC_OUTPUT_RAW_TEMPERATURE:
+            Serial.println("\ttemperature = " + String(output.signal));
+            break;
+        case BSEC_OUTPUT_RAW_PRESSURE:
+            Serial.println("\tpressure = " + String(output.signal));
+            break;
+        case BSEC_OUTPUT_RAW_HUMIDITY:
+            Serial.println("\thumidity = " + String(output.signal));
+            break;
+        case BSEC_OUTPUT_RAW_GAS:
+            Serial.println("\tgas resistance = " + String(output.signal));
+            break;
+        case BSEC_OUTPUT_RAW_GAS_INDEX:
+            Serial.println("\tgas index = " + String(output.signal));
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+void updateBsecState(Bsec2 bsec)
+{
+    static uint16_t stateUpdateCounter = 0;
+    bool update = false;
+
+    if (!stateUpdateCounter || (stateUpdateCounter * STATE_SAVE_PERIOD) < millis())
+    {
+        /* Update every STATE_SAVE_PERIOD minutes */
+        update = true;
+        stateUpdateCounter++;
+    }
+
+    if (update)
+    {
+        checkBsecStatus(bsec);
+    }
+}
+
+void checkBsecStatus(Bsec2 bsec)
+{
+    // BSEC
+    if (bsec.status < BSEC_OK)
+    {
+        Serial.println("BSEC error code : " + String(bsec.status));
+        errLeds(); /* Halt in case of failure */
+    }
+    else if (bsec.status > BSEC_OK)
+    {
+        Serial.println("BSEC warning code : " + String(bsec.status));
+    }
+
+    // Sensor
+    if (bsec.sensor.status < BME68X_OK)
+    {
+        Serial.println("BME68X error code : " + String(bsec.sensor.status));
+        errLeds(); /* Halt in case of failure */
+    }
+    else if (bsec.sensor.status > BME68X_OK)
+    {
+        Serial.println("BME68X warning code : " + String(bsec.sensor.status));
+    }
+}
