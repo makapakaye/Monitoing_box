d1e698c9a3ee58c158d6b69e885f53af0ccc31f8
hmes98318 <52697423+hmes98318@users.noreply.github.com>
2024-05-18 23:15:28 +0800

    feat: sensor_node deprecated bsec2

--- sensor_node/scripts/update_lib_win.bat
+++
@@ -1,4 +0,0 @@
-rmdir /s /q "%USERPROFILE%\Documents\Arduino\libraries\BME68x_Sensor_library"
-rmdir /s /q "%USERPROFILE%\Documents\Arduino\libraries\bsec2"
-
-xcopy /e /i "%USERPROFILE%\Documents\GitHub\Monitoing_box\sensor_node\lib\*" "%USERPROFILE%\Documents\Arduino\libraries\"
